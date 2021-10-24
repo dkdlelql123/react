714dac5ef1a88ed8177998f3a6645ab3665f09af
NamYeJi <dkdlelql123@naver.com>
2021-10-24 16:46:02 +0900

    react-query 설치

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { RecoilRoot, selector, atom, useRecoilState, useRecoilValue} from 'recoil'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import './index.css'
 import App from './App'
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
-    <RecoilRoot>
-      <App />
+    <RecoilRoot> 
+      <QueryClientProvider client={queryClient}>
+        <App /> 
+      </QueryClientProvider>
     </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root')
